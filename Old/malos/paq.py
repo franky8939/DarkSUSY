519a7f74ab0fe8a03ad859213968b99006e050ae
franky8939 <fmartiez8939@gmail.com>
2020-05-11 01:15:19 -0700

    ll

--- Old/malos/paq.py
+++
@@ -1,8 +0,0 @@
-def modificarLinea(archivo, buscar, reemplazar):
-    with open(archivo, "r") as f:
-        lines = (line.rstrip() for line in f)
-        altered_lines = [reemplazar if line == buscar else line for line in lines]
-
-    with open(archivo, "w") as f:
-        f.write('\n'.join(altered_lines) + '\n')
-
